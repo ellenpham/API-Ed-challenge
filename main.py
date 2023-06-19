6101c4798e475522d115c08d29b6d3c8aa624114
ellenpham <ellenpham1510@gmail.com>
2023-06-20 07:29:33 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os 
+from init import db, ma, bcrypt, jwt   
+
+# application factory - when flask run, by default, the create_app function will be run first
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+
+    # initialize libraries and connect them with the app 
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    return app
