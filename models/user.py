caac136d2ef5edcada7d52d89d7b0317bbec4e26
ellenpham <ellenpham1510@gmail.com>
2023-06-20 08:44:11 +1000

    Create CLI commands

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
     email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
     is_admin = db.Column(db.Boolean(), default=False)
