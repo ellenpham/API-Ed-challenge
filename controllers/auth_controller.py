abb1b8f7437c296119bffd46e32336b4d69d5f5b
ellenpham <ellenpham1510@gmail.com>
2023-06-20 11:30:44 +1000

    Create signin route

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -4,11 +4,12 @@ from models.user import User, user_schema, users_schema
 from flask_jwt_extended import create_access_token
 from sqlalchemy.exc import IntegrityError
 from psycopg2 import errorcodes
+from datetime import timedelta
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth_bp.route("/signup", methods=["POST"])
-def auth_register():
+def auth_signup():
     try:
         # pass data to the body of the request, which is done in Insomnia (front end)
         # extract the data in json structure to body {"name" : "User3", "email": "user2@email.com", "password": "user3pw"}
@@ -35,6 +36,23 @@ def auth_register():
             return {"error" : "Email address already exists"}, 409
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return {"error" : f'The {err.orig.diag.column_name} can not be blank'}, 409
+        
+@auth_bp.route('/signin', methods=["POST"])
+def auth_signin():
+    body_data = request.get_json()
+
+    # find the user by email address
+    # select data from database, filter the email which is matched with values passed from front end
+    stmt = db.select(User).filter_by(email=body_data.get("email"))
+    # get the single user
+    user = db.session.scalar(stmt)
+
+    # if user exists and password is correct
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        return { "email" : user.email, "token": token, "is_admin": user.is_admin }
+    else:
+        return { "error": "Invalid email or password" }, 401
     
 
 
